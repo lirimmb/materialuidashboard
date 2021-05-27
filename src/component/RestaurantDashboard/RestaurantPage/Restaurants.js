4770cf1bdedd9e1caebf4344db16b0a26ef46cd9
Lirim <lirimblakaj1@gmail.com>
2021-05-27 09:28:10 +0200

    restaurant component

--- src/component/RestaurantDashboard/RestaurantPage/Restaurants.js
+++ src/component/RestaurantDashboard/RestaurantPage/Restaurants.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined';
-import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import ResIMG from '../../../assets/restaurant.jpg';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -10,24 +8,29 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
 
 export class Restaurants extends Component {
-    state={
-        restaurants:[
+    state = {
+        restaurants: [
             {
                 restaurantID: 1,
                 restaurantName: 'Restaurant Test1',
-                dateCreated: '10/10/2011'
+                dateCreated: '10/10/2011',
+                location: 'Ulpjane',
             },
             {
                 restaurantID: 2,
                 restaurantName: 'Restaurant Test2',
-                dateCreated: '10/10/2012'
+                dateCreated: '10/10/2012',
+                location: 'Dardani'
             },
             {
                 restaurantID: 3,
                 restaurantName: 'Restaurant Test3',
-                dateCreated: '10/10/2013'
+                dateCreated: '10/10/2013',
+                location: 'Bregu Diellit'
             },
         ]
     }
@@ -35,14 +38,15 @@ export class Restaurants extends Component {
     render() {
         return (
             <div>
-                <h2 style={{color:'#8c000a', fontWeight:900}}>Restaurants</h2>
-                <TableContainer component={Paper}>
-                    <Table aria-label="simple table">
+                <h2 style={{ color: '#8c000a', fontWeight: 900 }}>Restaurants</h2>
+                <TableContainer component={Paper} style={{width:'90%'}}>
+                    <Table aria-label="simple table" className="res-table">
                         <TableHead>
                             <TableRow>
                                 <TableCell>#</TableCell>
                                 <TableCell >Restaurant Name</TableCell>
                                 <TableCell >Added Date</TableCell>
+                                <TableCell >Location</TableCell>
                                 <TableCell >Action</TableCell>
                             </TableRow>
                         </TableHead>
@@ -52,9 +56,15 @@ export class Restaurants extends Component {
                                     <TableCell ><Avatar alt="Restaurant" src={ResIMG} /></TableCell>
                                     <TableCell><strong>{res.restaurantName}</strong></TableCell>
                                     <TableCell><strong>{res.dateCreated}</strong></TableCell>
-                                    <TableCell >
-                                        <DeleteForeverIcon style={{ color: '#dc004e' }} />
-                                        <MoreVertOutlinedIcon />
+                                    <TableCell><strong>{res.location}</strong></TableCell>
+                                    <TableCell>
+                                        {/* action buttons*/}
+                                        <Button variant="contained" color="primary" style={{marginRight:'10px'}}>
+                                            Edit
+                                        </Button>
+                                        <Button variant="contained" color="secondary">
+                                            Delete
+                                        </Button>
                                     </TableCell>
                                 </TableRow>
                             ))}
